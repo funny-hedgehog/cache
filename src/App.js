2bb2bf9583816a3bd3040376508197df5fa83406
arifm <arifmho@yandex.ru>
2023-11-13 15:03:47 +0300

    fix 4

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import './App.css';
+import data from './data.json';
+import compass from './assets/compass.svg';
+import picture from './assets/picture.jpg';
 
 function App() {
   return (
     <div>
+      <h4>svg</h4>
+      <img src={compass} width={70} height={70} alt='svg' />
+      <h4>png</h4>
+      <img src={picture} width={512} height={341} alt='png' />
+      <p>
         рррррр
         vdnfdkfjkd
         fdljfkdflk
         dfdkjfkd
         fdljfkdlf
+      </p>
+      <ul>
+        {data.map(({title, id}) => {
+          return <li key={id}>{title}</li>
+        })}
+      </ul>
     </div>
   );
 }
